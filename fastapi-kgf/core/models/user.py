407fbd619d76d3028181c7b84c8af88b3aaf3ae0
Ramil Khakimov <bulstray@yandex.ru>
2026-03-11 18:06:37 +0300

    update imports

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from core.types import UserIdType
 
