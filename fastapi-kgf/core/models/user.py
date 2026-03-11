df4c5639e4feb8eb072bba0b1db8bbddc1044c67
Ramil Khakimov <bulstray@yandex.ru>
2026-03-11 18:00:47 +0300

    user_id type

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -2,14 +2,16 @@ from typing import TYPE_CHECKING
 
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
+from core.types import UserIdType
+
 from .base import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(SQLAlchemyBaseUserTable[int], Base):
+class User(SQLAlchemyBaseUserTable[UserIdType], Base):
 
     @classmethod
     def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
