1347ed3d9067712992de5bb060bd88c5c5ae96bb
Ramil Khakimov <bulstray@yandex.ru>
2026-03-11 17:18:43 +0300

    start app

---
+++ fastapi-kgf/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app_lifespan import lifespan
+
+app = FastAPI(
+    lifespan=lifespan,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
