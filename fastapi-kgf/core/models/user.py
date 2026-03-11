ccb3385bf95db0cf2f2ed8a571c0f40cd53f1eb9
Ramil Khakimov <bulstray@yandex.ru>
2026-03-11 17:43:16 +0300

    add method for users

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,7 +1,15 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
+
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
+        return SQLAlchemyUserDatabase(session, User)
