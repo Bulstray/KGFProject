11a3d563e5867d441250162028d5307c13d30f52
Ramil Khakimov <bulstray@yandex.ru>
2026-03-11 17:09:12 +0300

    main class for settings

--- fastapi-kgf/core/config.py
+++ fastapi-kgf/core/config.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
 
 class DatabaseConfig(BaseModel):
     url: str = "sqlite+aiosqlite:///database.db"
     echo: bool = False
+
+
+
+class Settings(BaseSettings):
+    database: DatabaseConfig = DatabaseConfig()
+
+settings = Settings()
