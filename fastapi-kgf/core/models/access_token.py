f46dc0e0e6f2b626f8a876f93e69f090c5949a44
Ramil Khakimov <bulstray@yandex.ru>
2026-03-12 13:38:02 +0300

    add field user_id

--- fastapi-kgf/core/models/access_token.py
+++ fastapi-kgf/core/models/access_token.py
@@ -4,7 +4,9 @@ from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyAccessTokenDatabase,
     SQLAlchemyBaseAccessTokenTable,
 )
+from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.types import UserIdType
 
@@ -15,6 +17,13 @@ if TYPE_CHECKING:
 
 
 class AccessToken(SQLAlchemyBaseAccessTokenTable[UserIdType], Base):
+
+    user_id: Mapped[UserIdType] = mapped_column(
+        Integer,
+        ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyAccessTokenDatabase(session, cls)
